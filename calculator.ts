5fdfa5644e744ed9b3c55973cfcb4ed8c6e9af47
AneelaTasneem <aneelatasneem90@gmail.com>
2024-03-15 08:44:01 -0700

    first commit

---
+++ calculator.ts
@@ -0,0 +1,25 @@
+// Improting inquirer:
+
+import inquirer from "inquirer";
+
+// creating a variavble for input1:
+ const input1 = await inquirer.prompt ({ 
+    name : "num1",
+    type : "number",
+    message: "Kindly enter your 1st number."
+
+ })
+
+ // creating a variavble for input2:
+ const input12= await inquirer.prompt ({ 
+    name : "num2",
+    type : "number",
+    message: "Kindly enter your 2nd number."
+
+ })
+
+//  Creating a variable of total of two values:
+let total: number = input1.num1 + input12.num2 
+
+// Printing Total:
+console.log(total);
